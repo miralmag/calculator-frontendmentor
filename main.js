d74efc63c6465084fba36e9dcae2ff294e5c495d
miralmag <miriam.almagro@gmail.com>
2022-10-31 14:16:25 +0100

    add querySelectors

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
 'use strict';
 
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[operation-number]');
+const deleteButton = document.querySelector('[data-delete]');
+const resetButton = document.querySelector('[data-reset]');
+const equalsButton = document.querySelector('[data-equals]');
+const result = document.querySelector('.screen-result');
+
 //toggle movement
 
 const toggleButtons = document.getElementsByClassName("toggle-button");
