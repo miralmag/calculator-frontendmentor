0164a9a4997b1819787acd17947cc97d6ee54112
miralmag <miriam.almagro@gmail.com>
2022-10-31 16:36:57 +0100

    add eventlisteners

--- main.js
+++ main.js
@@ -1,11 +1,61 @@
 'use strict';
 
+// class Calculator {
+//     sum(num1, num2){
+//         return num1 + num2; 
+//     }
+
+//     subtract(num1, num2){
+//         return num1 - num2; 
+//     }
+
+//     multiply(num1, num2){
+//         return num1 * num2; 
+//     }
+
+//     divide (num1, num2){
+//         return num1 / num2; 
+//     }
+// }
+
+// class Screen{
+//     constructor(screenValue){
+//         this.screenValue = screenValue;
+//         this.calculator = new Calculator();
+//         this.currentValue = '';
+//         this.previousValue = '';
+//     }
+//     addNumber(number){
+//         this.currentValue = number;
+//     }
+// }
+
+
 const numberButtons = document.querySelectorAll('[data-number]');
-const operationButtons = document.querySelectorAll('[operation-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
 const deleteButton = document.querySelector('[data-delete]');
 const resetButton = document.querySelector('[data-reset]');
 const equalsButton = document.querySelector('[data-equals]');
-const result = document.querySelector('.screen-result');
+const screenResult = document.querySelector('.screen-result');
+
+// const screen = new Screen(result);
+
+resetButton.addEventListener( 'click', () => {
+    screenResult.innerHTML = "";
+})
+
+numberButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        screenResult.innerHTML += button.id;
+    })
+})
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () =>{
+        screenResult.innerHTML += button.id;
+    } )
+})
+
 
 //toggle movement
 
@@ -14,14 +64,14 @@ const toggleArr = [...toggleButtons];
 
 toggleArr.forEach((element, index) => {
     element.addEventListener("click", () => {
-      element.style.opacity = "1";
+        element.style.opacity = "1";
 
-      toggleArr.filter(function (item) {
-        return item != element;
-      })
-      .forEach((item) => {
-        item.style.opacity = "0";
-      });
-  });
+        toggleArr.filter(function (item) {
+            return item != element;
+        })
+        .forEach((item) => {
+            item.style.opacity = "0";
+        });
+    });
 });
 
