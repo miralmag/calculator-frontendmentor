918ad65ddb91c596a60346f8d22421b82c60e298
miralmag <miriam.almagro@gmail.com>
2022-11-02 12:28:52 +0100

    add themes styles

--- main.js
+++ main.js
@@ -126,3 +126,64 @@ toggleArr.forEach((element, index) => {
     });
 });
 
+//toggle themes
+
+function handleThemes (ev) {
+ev.preventDefault;
+if (ev.currentTarget.id === 'two') {
+    document.documentElement.style.setProperty('--main-bg', 'hsl(0, 0%, 90%)');
+    document.documentElement.style.setProperty('--keypad-bg', 'hsl(0, 5%, 81%)');
+    document.documentElement.style.setProperty('--screen-bg', 'hsl(0, 0%, 93%)');
+    document.documentElement.style.setProperty('--toggle-btn', 'hsl(25, 98%, 40%)');
+    document.documentElement.style.setProperty('--del-reset-bg', 'hsl(185, 42%, 37%)');
+    document.documentElement.style.setProperty('--del-reset-shadow', 'hsl(185, 58%, 25%)');
+    document.documentElement.style.setProperty('--equals-bg', 'hsl(25, 98%, 40%)');
+    document.documentElement.style.setProperty('--equals-shadow', 'hsl(25, 99%, 27%)');
+    document.documentElement.style.setProperty('--key-bg', 'hsl(45, 7%, 89%)');
+    document.documentElement.style.setProperty('--key-shadow', 'hsl(35, 11%, 61%)');
+    document.documentElement.style.setProperty('--result-color', 'hsl(60, 10%, 19%)');
+    document.documentElement.style.setProperty('--text2', 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty('--text1', 'hsl(60, 10%, 19%)');
+    document.documentElement.style.setProperty('--text-header', 'hsl(60, 10%, 19%)');
+    document.documentElement.style.setProperty('--equals-text', 'hsl(0, 0%, 100%)');
+}
+else if (ev.currentTarget.id === 'one') {
+    document.documentElement.style.setProperty('--main-bg', 'hsl(222, 26%, 31%)');
+    document.documentElement.style.setProperty('--keypad-bg', 'hsl(223, 31%, 20%)');
+    document.documentElement.style.setProperty('--screen-bg', 'hsl(224, 36%, 15%)');
+    document.documentElement.style.setProperty('--toggle-btn', 'hsl(6, 63%, 50%)');
+    document.documentElement.style.setProperty('--del-reset-bg', 'hsl(225, 21%, 49%)');
+    document.documentElement.style.setProperty('--del-reset-shadow', 'hsl(224, 28%, 35%)');
+    document.documentElement.style.setProperty('--equals-bg', 'hsl(6, 63%, 50%)');
+    document.documentElement.style.setProperty('--equals-shadow', 'hsl(6, 70%, 34%)');
+    document.documentElement.style.setProperty('--key-bg', 'hsl(30, 25%, 89%)');
+    document.documentElement.style.setProperty('--key-shadow', 'hsl(28, 16%, 65%)');
+    document.documentElement.style.setProperty('--result-color', 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty('--text2', 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty('--text1', 'hsl(221, 14%, 31%)');
+    document.documentElement.style.setProperty('--text-header', 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty('--equals-text', 'hsl(0, 0%, 100%)');
+}
+else if (ev.currentTarget.id === 'three') {
+    document.documentElement.style.setProperty('--main-bg', 'hsl(268, 75%, 9%)');
+    document.documentElement.style.setProperty('--keypad-bg', 'hsl(268, 71%, 12%)');
+    document.documentElement.style.setProperty('--screen-bg', 'hsl(268, 71%, 12%)');
+    document.documentElement.style.setProperty('--toggle-btn', 'hsl(176, 100%, 44%)');
+    document.documentElement.style.setProperty('--del-reset-bg', 'hsl(281, 89%, 26%)');
+    document.documentElement.style.setProperty('--del-reset-shadow', 'hsl(285, 91%, 52%)');
+    document.documentElement.style.setProperty('--equals-bg', 'hsl(176, 100%, 44%)');
+    document.documentElement.style.setProperty('--equals-shadow', 'hsl(177, 92%, 70%)');
+    document.documentElement.style.setProperty('--key-bg', 'hsl(268, 47%, 21%)');
+    document.documentElement.style.setProperty('--key-shadow', 'hsl(290, 70%, 36%)');
+    document.documentElement.style.setProperty('--result-color', 'hsl(52, 100%, 62%)');
+    document.documentElement.style.setProperty('--text2', 'hsl(0, 0%, 100%)');
+    document.documentElement.style.setProperty('--text1', 'hsl(52, 100%, 62%)');
+    document.documentElement.style.setProperty('--text-header', 'hsl(52, 100%, 62%)');
+    document.documentElement.style.setProperty('--equals-text', 'hsl(198, 20%, 13%);');
+}
+}
+
+toggleArr.forEach((element, index) => {
+    element.addEventListener("click", handleThemes)
+})
+
